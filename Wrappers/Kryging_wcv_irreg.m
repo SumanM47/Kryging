6f8a4da98d76d06e41a3509875233671a5aef53d
SumanM47 <smajumd2@ncsu.edu>
2020-12-23 19:54:55 +0530

    Update Kryging_wcv_irreg.m

--- Wrappers/Kryging_wcv_irreg.m
+++ Wrappers/Kryging_wcv_irreg.m
@@ -1,16 +1,18 @@
 function [x_est, y_est, theta_est, numit, tim, xl, xu, yl, yu] = Kryging_wcv_irreg(d,s,A,nu,X,Initmat,k,nvec)
 
 ns = size(s,1);
-[~,sort_ind] = sortrows(s);
-W = sparse(sort_ind,(1:ns),ones(ns,1),ns,ns);
-Anew = A*W;
-R = speye(ns);
+dmaxx = 1; dmaxy = 1;
+xmin = min(s);
+xmax = max(s);
+[W1,news,newxmin,newxmax,newnvec] = createA(s,xmin,xmax,nvec,dmaxx,dmaxy);
+newns = size(news,1);
+[~,sort_ind] = sortrows(news);
+W = W1*sparse(sort_ind,(1:newns),ones(newns,1),newns,newns);
+Anew = A*W;R = speye(ns);
 
 X = [ones(ns,1) X];
 
-xmin = min(s);
-xmax = max(s);
-nvec = [size(unique(s(:,1)),1) size(unique(s(:,2)),1)];
+%nvec = [size(unique(s(:,1)),1) size(unique(s(:,2)),1)];
 
 theta = [1 1];
 

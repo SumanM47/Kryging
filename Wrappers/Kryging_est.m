052d3198c6d0d42c374e1fab0dc20814c9db6175
SumanM47 <smajumd2@ncsu.edu>
2020-12-24 11:02:27 +0530

    Update Kryging_est.m

--- Wrappers/Kryging_est.m
+++ Wrappers/Kryging_est.m
@@ -1,5 +1,48 @@
 function [est_x, est_y, est_theta, numit, xl,xu,yl,yu] = Kryging_est(bhat,sill_init,range_init,nu_opt,sigma_e_init,d,X,nmi,xmin,xmax,nvec,A,R,maxit,theta,W)
 
+%
+%function to estimate the latent true state vector x, 
+%    predictions of the observed process at prediction locations,
+%    the mean and spatial parameters,
+%    upper and lower bounds for x and y processes
+%	
+%Usage:
+%[est_x,est_y,est_theta,numit] = Kryging_est(bhat,sill_init,range_init,nu_opt,sigma_e_init,d,X,nmi,xmin,xmax,nvec,A,R,maxit,theta,W)
+%[est_x, est_y, est_theta, numit, xl,xu,yl,yu] = Kryging_est(bhat,sill_init,range_init,nu_opt,sigma_e_init,d,X,nmi,xmin,xmax,nvec,A,R,maxit,theta,W)
+%
+%Input:
+%bhat: initial estimates for the mean parameters
+%sill_init: initial estimate for the partial sill
+%range_init: initial estimate for the spatial range parameter
+%nu_opt: smoothness parameter for the Matern covariance function (fixed)
+%sigma_e_init: initial estimate for the nugget parameter
+%d: vector of observations
+%X: matrix of covariates
+%nmi: indices of non-missing observations
+%xmin: vector of minimum of the domain in both coordinates
+%xmax: vector of maximum of the domain in both coordinates
+%nvec: vector of number of unique points in each coordinate
+%A: matrix of relationship between Y and the latent true state values
+%R: diagonal matrix of observational correlation, typically identity
+%maxit: order of Krylov subspace
+%theta: vector of scale anisotropy parameters-- [1 1] is isotropic
+%W: matrix of weights-- typically a permutation matrix for gridded data and a Wendland weight based matrix for irregular data
+%
+%Output:
+%est_x: estimated values of the true state process 
+%est_y: estimated and predicted values of the observed process
+%est_theta: estimated mean and spatial parameters
+%numit: number of iterations required for the optimization process
+%xl: lower 95% confidence limit for the true state process estimates (optional)
+%xu: upper 95% confidence limit for the true state process estimates (optional)
+%yl: lower 95% confidence limit for the observed process estimates (optional)
+%yu: upper 95% confidence limit for the observed process estimates (optional)
+%
+%
+%Written for and used in "Kryging: Geostatistical analysis of massive spatial datasets using Krylov subspaces" - Majumder et al. (2020+)
+%
+
+
 bhatvec = bhat(:);
 
 R_nm = R(nmi,nmi); A_nm = A(nmi,:); dnm = d(nmi);
